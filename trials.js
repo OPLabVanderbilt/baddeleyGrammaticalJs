641462afc74ac5dc973ebb44f2bee3e1992bd91f
Conor Smithson <52662469+Conzcode@users.noreply.github.com>
2023-04-25 20:58:38 -0500

    Create test

---
+++ trials.js
@@ -0,0 +1,163 @@
+trials = [
+    {
+        Trial: 1,
+        Stim: `<p>A is preceded by B: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 2,
+        Stim: `<p>A is not preceded by B: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 3,
+        Stim: `<p>B follows A: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 4,
+        Stim: `<p>B is not preceded by A: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 5,
+        Stim: `<p>B is followed by A: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'True'
+    },
+    
+    {
+        Trial: 6,
+        Stim: `<p>B is not followed by A: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 7,
+        Stim: `<p>A precedes B: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 8,
+        Stim: `<p>A does not follow B: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 9,
+        Stim: `<p>B does not follow A: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 10,
+        Stim: `<p>B is not preceded by A: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 11,
+        Stim: `<p>A is followed by B: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 12,
+        Stim: `<p>A is not preceded by B: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 13,
+        Stim: `<p>A is not followed by B: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 14,
+        Stim: `<p>A does not precede B: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 15,
+        Stim: `<p>A follows B: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 16,
+        Stim: `<p>A is preceded by B: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 17,
+        Stim: `<p>B does not precede A: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 18,
+        Stim: `<p>B is preceded by A: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 19,
+        Stim: `<p>A is followed by B: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 20,
+        Stim: `<p>A follows B: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 21,
+        Stim: `<p>A does not precede B: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 22,
+        Stim: `<p>B is followed by A: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 23,
+        Stim: `<p>A does not follow B: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 24,
+        Stim: `<p>B is preceded by A: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 25,
+        Stim: `<p>A is not followed by B: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 26,
+        Stim: `<p>B is not followed by A: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 27,
+        Stim: `<p>B does not follow A: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 28,
+        Stim: `<p>A precedes B: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 29,
+        Stim: `<p>B does not precede A: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 30,
+        Stim: `<p>B precedes A: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'True'
+    },
+    {
+        Trial: 31,
+        Stim: `<p>B precedes A: &emsp;<b>AB</b></p><br>`,
+        CorrRes: 'False'
+    },
+    {
+        Trial: 32,
+        Stim: `<p>B follows A: &emsp;<b>BA</b></p><br>`,
+        CorrRes: 'False'
+    }    
+]
